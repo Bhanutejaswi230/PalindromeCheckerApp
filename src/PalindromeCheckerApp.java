c7f2e0291bb1620ed318ce1438b9d0b32b9ba69d
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-02-25 10:56:58 +0530

    Base application setup: consolidated PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,28 @@
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    // UC1 - Welcome Message
+    public static void UC1() {
+        System.out.println("Welcome to Palindrome Checker App!");
+    }
 
-        // UC1 – Welcome Message
-        System.out.println("   Welcome to Palindrome Checker   ");
-        // UC2 – Hardcoded Palindrome Check
+    // UC2 - Hardcoded Palindrome
+    public static void UC2() {
         String word = "madam";
-
         if(word.equals("madam")) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
         }
     }
-}
+
+    // UC3 - String Reverse
+    public static void UC3() {
+        String word = "racecar";
+        String reversed = "";
+        for(int i = word.length()-1; i >=0; i--) {
+            reversed += word.charAt(i);
+        }
+        System.out.println(word + " is " + (word.equals(reversed) ? "" : "NOT ") + "Palindrome");
+    }
+
+  
