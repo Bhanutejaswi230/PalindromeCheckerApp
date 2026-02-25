93d54a7bff122023988ab2aff882167fe698bcea
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-02-25 11:04:36 +0530

    UC1: Welcome Message implemented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,5 @@
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-
-        // UC1 – Welcome Message
-        System.out.println("   Welcome to Palindrome Checker   ");
-        // UC2 – Hardcoded Palindrome Check
-        String word = "madam";
-
-        if(word.equals("madam")) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is not a Palindrome");
-        }
+        System.out.println("Welcome to Palindrome Checker App");
     }
 }
