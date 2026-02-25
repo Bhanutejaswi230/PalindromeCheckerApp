25fb50b003a8d3578db6c5b16bd687a36c6344a5
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-02-25 09:09:12 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,16 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+
+        // UC1 – Welcome Message
+        System.out.println("   Welcome to Palindrome Checker   ");
+        // UC2 – Hardcoded Palindrome Check
+        String word = "madam";
+
+        if(word.equals("madam")) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
     }
 }
