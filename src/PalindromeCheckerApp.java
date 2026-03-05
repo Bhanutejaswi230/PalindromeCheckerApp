92165a348284c0ae3848dbc591b1c26597b3250b
Bhanutejaswi230 <garinebhanutejaswi@gmail.com>
2026-03-05 09:52:56 +0530

    UC12: Strategy pattern for palindrome algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,46 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 public class PalindromeCheckerApp {
-    // UC11: Object-Oriented Palindrome Service
-    static class PalindromeChecker {
-        public boolean checkPalindrome(String str) {
-            java.util.Stack<Character> stack = new java.util.Stack<>();
-            for (int i = 0; i < str.length(); i++) {
-                stack.push(str.charAt(i));
+    interface PalindromeStrategy {
+        boolean isPalindrome(String str);
+    }
+    static class StackStrategy implements PalindromeStrategy {
+        @Override
+        public boolean isPalindrome(String str) {
+            Stack<Character> stack = new Stack<>();
+            for (char ch : str.toCharArray()) stack.push(ch);
+            for (char ch : str.toCharArray()) {
+                if (ch != stack.pop()) return false;
             }
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) != stack.pop()) {
-                    return false;
-                }
-            }
+            return true;
+        }
+    }
+    static class DequeStrategy implements PalindromeStrategy {
+        @Override
+        public boolean isPalindrome(String str) {
+            Deque<Character> deque = new LinkedList<>();
+            for (char ch : str.toCharArray()) deque.addLast(ch);
 
+            while (deque.size() > 1) {
+                if (deque.removeFirst() != deque.removeLast()) return false;
+            }
             return true;
         }
     }
     public static void main(String[] args) {
         String original = "madam";
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(original);
-        if (result) {
+        PalindromeStrategy strategy;
+        strategy = new StackStrategy();
+        System.out.println("Using StackStrategy:");
+        if (strategy.isPalindrome(original)) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
+        }
+        strategy = new DequeStrategy();
+        System.out.println("\nUsing DequeStrategy:");
+        if (strategy.isPalindrome(original)) {
             System.out.println(original + " is a Palindrome");
         } else {
             System.out.println(original + " is NOT a Palindrome");
